c77caecce1d86afca53389b4e4bdb7b6134f1b8b
Philippe Mols <philippe_mols@hotmail.com>
2021-08-24 19:43:19 +0700

    Set-up viewcontrollers and main storyboard

---
+++ DPR Application/DataloggingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DataloggingViewController.swift
+//  DPR Application
+//
+//  Created by Philippe Mols on 24/08/21.
+//  Copyright © 2021 Les Deux Paks. All rights reserved.
+//
+
+import UIKit
+
+class DataloggingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DPR Application/ViewController.swift
+++ DPR Application/ViewController.swift
@@ -15,55 +15,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func ExportCSV(_ sender: Any) {
-        
-        let sFilename = "test.csv"
-       
-        let documentDirectoryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
-        
-        let documentURL = URL(fileURLWithPath: documentDirectoryPath).appendingPathComponent(sFilename)
-       
-        let output = OutputStream.toMemory()
-       
-        let csvWriter = CHCSVWriter(outputStream: output, encoding: String.Encoding.utf8.rawValue, delimiter: ",".utf16.first!)
-        
-        //Header of the CSV file
-        csvWriter?.writeField("Date")
-        csvWriter?.writeField("Location")
-        csvWriter?.writeField("Start time")
-        csvWriter?.writeField("End time")
-        csvWriter?.writeField("Duration")
-        csvWriter?.writeField("Activity code")
-        csvWriter?.writeField("Activity name")
-        csvWriter?.finishLine()
-        
-        //Array to add data for the Activities
-        var arrayOfActivities = [[String]]()
-        arrayOfActivities.append(["01/09/2021","D12","15:00","18:00","03:00","LO1","Load-out JKT"])
-        arrayOfActivities.append(["02/09/2021","D07","13:00","17:00","04:00","LO1","Load-out JKT"])
-        arrayOfActivities.append(["18/09/2021","D09","15:00","18:00","03:00","LO1","Load-out JKT"])
-        
-        for(elements) in arrayOfActivities.enumerated(){
-            csvWriter?.writeField(elements.element[0]) //added Date
-            csvWriter?.writeField(elements.element[1]) //added Location
-            csvWriter?.writeField(elements.element[2]) //added Start time
-            csvWriter?.writeField(elements.element[3]) //added End time
-            csvWriter?.writeField(elements.element[4]) //added duration
-            csvWriter?.writeField(elements.element[5]) //added activity code
-            csvWriter?.writeField(elements.element[6]) //added activity name
-            csvWriter?.finishLine()
+    @IBAction func tapDataloggingButton() {
+        guard let dataloggingvc = storyboard?.instantiateViewController(identifier: "datalogging" ) as? DataloggingViewController else {
+            return
         }
-        csvWriter?.closeStream()
         
-        let buffer = (output.property(forKey: .dataWrittenToMemoryStreamKey) as? Data)!
-        
-        do {
-            try buffer.write(to: documentURL)
-        }
-        catch{
-            
-        }
+        present(dataloggingvc, animated: true)
     }
     
+    @IBAction func tapPerformanceButton() {
+        guard let performancevc = storyboard?.instantiateViewController(identifier: "performance" ) as? PerformanceViewController else {
+            return
+        }
+        
+        present(performancevc, animated: true)
+    }
+    
+    @IBAction func tapImportListButton() {
+        guard let importlistvc = storyboard?.instantiateViewController(identifier: "importlist" ) as? ImportListViewController else {
+            return
+        }
+        
+        present(importlistvc, animated: true)
+    }
+    
+    @IBAction func tapReportingButton() {
+        guard let reportingvc = storyboard?.instantiateViewController(identifier: "reporting" ) as? ReportingViewController else {
+            return
+        }
+        
+        present(reportingvc, animated: true)
+    }
 }
 
